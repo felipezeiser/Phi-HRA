a7ea187f66fbf0ac08f3b63767f795ea56b20af1
Felipe André Zeiser <felipe_zei@hotmail.com>
2024-03-04 14:45:12 -0300

    Update urls.py

--- phihra/phihra/urls.py
+++ phihra/phihra/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profissional/', include('profissional.urls')),
-    path('paciente/', include('paciente.urls')),
+    path('', include('paciente.urls')),
     
     
 ]
